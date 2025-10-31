e71f655b393568cdbb1497bcc63de2d9acd7a54e
Matt Titus <mtitus83@gmail.com>
2025-10-31 01:21:50 -0400

    Include PID settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -665,13 +665,15 @@
   #if ENABLED(PID_PARAMS_PER_HOTEND)
     // Specify up to one value per hotend here, according to your setup.
     // If there are fewer values, the last one applies to the remaining hotends.
+
     #define DEFAULT_Kp_LIST {  22.20,  22.20 }
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    // PETG: Fan 15%, Temp 240c
+    #define DEFAULT_Kp  28.76
+    #define DEFAULT_Ki   2.85
+    #define DEFAULT_Kd  72.52
   #endif
 #endif
 
@@ -754,9 +756,11 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 41.78
-  #define DEFAULT_bedKi 7.32
-  #define DEFAULT_bedKd 158.93
+  //
+  // Bed Temp: 70c, Fan: 15%
+  #define DEFAULT_bedKp 92.79
+  #define DEFAULT_bedKi 17.84
+  #define DEFAULT_bedKd 321.66
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
